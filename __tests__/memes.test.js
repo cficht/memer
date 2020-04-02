0e351335c3a9db923db0118813632657823e1291
cficht <chris.ficht@gmail.com>
2020-04-02 09:50:46 -0700

    tested and wrote memes delete by id route

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -56,4 +56,13 @@ describe('memes routes', () => {
       });
   });
 
+  it('deletes a meme', async() => {
+    const meme = await getMeme();
+    return request(app)
+      .delete(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual(meme);
+      });
+  });
+
 });
