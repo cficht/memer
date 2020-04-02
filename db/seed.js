b65d0109fa9e3ac74ab340554ca0a0dcea1cac7a
cficht <chris.ficht@gmail.com>
2020-04-02 11:44:59 -0700

    added random meme service module to get random memes from an api

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,13 @@
 const Meme = require('../lib/models/Meme');
 const chance = require('chance').Chance();
+const getMemes = require('../lib/services/random-memes');
 
 module.exports = async({ memesToCreate = 100 } = {}) => {
-
-  const images = [
-    'https://images7.memedroid.com/images/UPLOADED113/5618cd9195cf5.jpeg',
-    'https://images7.memedroid.com/images/UPLOADED143/5618cd6b07685.jpeg',
-    'https://images7.memedroid.com/images/UPLOADED136/5618c4b461dc4.jpeg',
-    'https://images7.memedroid.com/images/UPLOADED123/559e176e7a719.jpeg',
-    'https://images7.memedroid.com/images/UPLOADED172/559dc3f61577b.jpeg'
-  ];
-
-  await Meme.create([...Array(memesToCreate)].map(() => ({
-    top: chance.animal(),
-    image: chance.pickone(images),
-    bottom: chance.profession()
+  const memes = await getMemes();
+  await Meme.create([...Array(memesToCreate)].map((tag, index) => ({
+    top: memes[index].name,
+    image: memes[index].url,
+    bottom: chance.sentence()
   })));
 };
 
